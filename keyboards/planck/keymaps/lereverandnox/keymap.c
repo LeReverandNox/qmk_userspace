d8086a9dbe5c833dad0c339c6a83dbbe376a3aad
lereverandnox <lereverandnox@gmail.com>
2019-09-07 14:47:40 +0200

    Add EEP_RST binding

--- keyboards/planck/keymaps/lereverandnox/keymap.c
+++ keyboards/planck/keymaps/lereverandnox/keymap.c
@@ -202,18 +202,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------
  * |Mu Tog|Ck Tog|Au Tog| RGB  |RGBMOD| HUE+ | HUE- | SAT+ | SAT- |BRGTH+|BRGTH-|Reset |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Mu Mod|Ck Rst|MidiTo|Ag Tog|NKRO T|      |      |      |      |      |      |Debug |
+ * |Mu Mod|Ck Rst|MidiTo|Ag Tog|NKRO T|      |      |      |      |      |      |EepRst|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Voice+|Ck P+ |      |      |      |      |      |      |      |      |      |Dvorak|
+ * |Voice+|Ck P+ |      |      |      |      |      |      |      |      |      |Debug |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Voice-|Ck P- |      |      |      |      |      |      |      |      |Plover|Qwerty|
+ * |Voice-|Ck P- |      |      |      |      |      |      |      |Plover|Qwerty|Dvorak|
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
     MU_TOG, CK_TOGG, AU_TOG,  RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, RESET,
-    MU_MOD, CK_RST,  MI_TOG,  AG_TOGG, NKR_TOG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, DEBUG,
-    MUV_IN, CK_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, DVORAK,
-    MUV_DE, CK_DOWN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, PLOVER,  QWERTY
+    MU_MOD, CK_RST,  MI_TOG,  AG_TOGG, NKR_TOG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, EEP_RST,
+    MUV_IN, CK_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, DEBUG,
+    MUV_DE, CK_DOWN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  PLOVER,  QWERTY,  DVORAK
     ),
 
 /* Music
