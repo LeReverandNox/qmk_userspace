05bbf34489ff5ef4f86add3bd10d0ee1211316a8
LRN <lereverandnox@gmail.com>
2019-09-13 01:03:38 +0200

    Add comments in keycodes.h

--- keyboards/planck/keymaps/lereverandnox/keycodes.h
+++ keyboards/planck/keymaps/lereverandnox/keycodes.h
@@ -8,6 +8,7 @@ enum planck_keycodes {
   KC_MAKE
 };
 
+// Layers
 #define NAV      MO(_NAV)
 #define SYM      MO(_SYM)
 #define NUM      MO(_NUM)
@@ -16,6 +17,16 @@ enum planck_keycodes {
 #define MOUSE_T  TG(_MOUSE)
 #define MEDIA    MO(_MEDIA)
 
+// Layer Tap
+#define LT_ESC   LT(NAV, KC_ESC)
+#define LT_SPC   LT(SYM, KC_SPC)
+#define LT_TAB   LT(NUM, KC_TAB)
+#define LT_ENT   LT(NUM, KC_ENT)
+#define LT_BSPC  LT(SYM, KC_BSPC)
+#define LT_RALT  LT(FN,  KC_RALT)
+
+// Home mods
+// DVORAK
 #define HOME_AD  CTL_T(KC_A)
 #define HOME_OD  LGUI_T(KC_O)
 #define HOME_ED  LALT_T(KC_E)
@@ -24,7 +35,7 @@ enum planck_keycodes {
 #define HOME_TD  LALT_T(KC_T)
 #define HOME_ND  RGUI_T(KC_N)
 #define HOME_SD  RCTL_T(KC_S)
-
+// QWERTY
 #define HOME_AQ  CTL_T(KC_A)
 #define HOME_SQ  LGUI_T(KC_S)
 #define HOME_DQ  LALT_T(KC_D)
@@ -34,16 +45,12 @@ enum planck_keycodes {
 #define HOME_LQ  RGUI_T(KC_L)
 #define HOME_SC  RCTL_T(KC_SCLN)
 
-#define LT_ESC   LT(NAV, KC_ESC)
-#define LT_SPC   LT(SYM, KC_SPC)
-#define LT_TAB   LT(NUM, KC_TAB)
-#define LT_ENT   LT(NUM, KC_ENT)
-#define LT_BSPC  LT(SYM, KC_BSPC)
-#define LT_RALT  LT(FN,  KC_RALT)
+// Ose-shot mods
 #define OS_LALT  OSM(MOD_LALT)
 #define OS_RGUI  OSM(MOD_RGUI)
 #define OS_RCTL  OSM(MOD_RCTL)
 
+// Tapdances
 #define TD_DOT   TD(_DOT)
 
 #define NKR_TOG MAGIC_TOGGLE_NKRO
