725e046df903564451b04ca2d38be9ebb6e05fff
LRN <lereverandnox@gmail.com>
2019-09-14 14:06:41 +0200

    Add two defines for OSM shifts

--- keyboards/planck/keymaps/lereverandnox/keycodes.h
+++ keyboards/planck/keymaps/lereverandnox/keycodes.h
@@ -118,9 +118,8 @@ enum planck_keycodes {
 #endif
 
 // Ose-shot mods
-#define OS_LALT  OSM(MOD_LALT)
-#define OS_RGUI  OSM(MOD_RGUI)
-#define OS_RCTL  OSM(MOD_RCTL)
+#define OS_LSFT OSM(MOD_LSFT)
+#define OS_RSFT OSM(MOD_RSFT)
 
 // Tapdances
 #define TD_DOT   TD(_DOT)
