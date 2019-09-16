2687afdbf027092a4aa820b6b32ab3dffc60c96b
LRN <lereverandnox@gmail.com>
2019-09-16 13:45:31 +0200

    Move layers enum in a separate file

---
+++ keyboards/planck/keymaps/lereverandnox/layers.h
@@ -0,0 +1,15 @@
+#pragma once
+
+enum planck_layers {
+  _QWERTY = 0,
+  _DVORAK,
+  _MOUSE,
+  _NAV,
+  _SYM,
+  _NUM,
+  _FN,
+  _PLOVER,
+  _MUSIC,
+  _MEDIA,
+  _ADJUST
+};
--- keyboards/planck/keymaps/lereverandnox/lereverandnox.h
+++ keyboards/planck/keymaps/lereverandnox/lereverandnox.h
@@ -2,20 +2,7 @@
 
 #include "planck.h"
 
-#include "helpers.h"
+#include "layers.h"
 #include "keycodes.h"
 #include "tapdances.h"
-
-enum planck_layers {
-  _QWERTY = 0,
-  _DVORAK,
-  _MOUSE,
-  _NAV,
-  _SYM,
-  _NUM,
-  _FN,
-  _PLOVER,
-  _MUSIC,
-  _MEDIA,
-  _ADJUST
-};
+#include "helpers.h"
