b0f48fc8fcf90c568b657173ca7daa022e7a6ca0
LRN <lereverandnox@gmail.com>
2019-09-18 21:56:07 +0200

    Put back mods on nav layer

--- keyboards/planck/keymaps/lereverandnox/layers/nav_layer.h
+++ keyboards/planck/keymaps/lereverandnox/layers/nav_layer.h
@@ -51,7 +51,7 @@
 [_NAV] = LAYOUT_planck_grid(
     ALT_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGUP,  XXXXXXX, XXXXXXX, _______,
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, _______,
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_END,  KC_PGDN,  XXXXXXX, XXXXXXX, _______,
+    _______, KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, XXXXXXX, XXXXXXX, KC_END,  KC_PGDN,  XXXXXXX, XXXXXXX, _______,
     _______, _______, _______, _______, _______, _______, _______, KC_INS,  _______,  _______, _______, _______
     ),
 #endif
