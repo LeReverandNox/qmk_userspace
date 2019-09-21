f7806d5f2ff12dc98f01ec4fe22d57a726c27f4c
LRN <lereverandnox@gmail.com>
2019-09-22 01:57:54 +0200

    improve behavior of tap_shift_code

--- keyboards/planck/keymaps/lereverandnox/helpers.c
+++ keyboards/planck/keymaps/lereverandnox/helpers.c
@@ -69,21 +69,28 @@ void symbol_pair(uint8_t shift, uint16_t left, uint16_t right)
 }
 
 void tap_shift_caps(qk_tap_dance_state_t *state, uint8_t shift) {
+    uint8_t mods = get_mods();
+    uint8_t os_mods = get_oneshot_mods();
+
     // double tap: toggle Capslock
     if (state->count >= 2) {
-        tap_key(KC_CAPS);
-        is_caps_on = !is_caps_on;
+        // To avoid switching romaji/hiragana with Windows IME (Shift + Caps)
+        if (!(mods & MOD_MASK_SHIFT)) {
+            tap_key(KC_CAPS);
+            is_caps_on = !is_caps_on;
+        }
     // pressed: hold Shift
     } else if (state->pressed) {
         register_code(shift);
     // single tap
     } else {
-        //  if enable, turn Capslock off
-        if (is_caps_on) {
+        //  if enable, turn Capslock off. Prevent Shift + Caps
+        if (is_caps_on && !(mods & MOD_MASK_SHIFT) && !(os_mods & MOD_MASK_SHIFT)) {
             tap_key(KC_CAPS);
             is_caps_on = false;
-        // oneshot Shift (don't know why it doesn't work with MOD_RSFT...)
-        } else {
+        } else if (os_mods & MOD_MASK_SHIFT) { // Disable OS Shift if enable
+            set_oneshot_mods(mods ^ MOD_LSFT);
+        } else { // oneshot Shift (don't know why it doesn't work with MOD_RSFT...)
             set_oneshot_mods(MOD_LSFT);
         }
     }
