ab3307a1136f950ee6f45d08f73c5a0784670d94
LeReverandNox <lereverandnox@gmail.com>
2019-12-29 18:19:22 +0100

    Edit Qwerty layer to match Dvorak

--- keyboards/planck/keymaps/lereverandnox/layers/qwerty_layer.h
+++ keyboards/planck/keymaps/lereverandnox/layers/qwerty_layer.h
@@ -24,19 +24,19 @@
 // +-------+------+-----+-----+-------+-----+-------+-------+------+-----+------+-------+
 // |   `   |   Q  |  W  |  E  |   R   |  T  |   Y   |   U   |   I  |  O  |   P  | Mouse |
 // +-------+------+-----+-----+-------+-----+-------+-------+------+-----+------+-------+
-// |  Tab  |   A  |  S  |  D  |   F   |  G  |   H   |   J   |  K   |  L  |   ;  |       |
+// |  Esc  |   A  |  S  |  D  |   F   |  G  |   H   |   J   |  K   |  L  |   ;  | Enter |
 // | Media |      |     |     |       |     |       |       |      |     |      |       |
 // +-------+------+-----+-----+-------+-----+-------+-------+------+-----+------+-------+
 // | Shift |   Z  |  X  |  C  |   V   |  B  |   N   |   M   |   ,  |  .  |   /  | Shift |
 // |       | Ctrl | GUI | Alt | Shift |     |       | Shift |  Alt | GUI | Ctrl |       |
 // +-------+------+-----+-----+-------+-----+-------+-------+------+-----+------+-------+
-// |  Ctrl |  GUI | Alt | Tab | Space | Esc | Enter | Bkspc | RAlt | Alt |  GUI |  Ctrl |
-// |       |      |     | Nav |  Num  | Sym |  Num  |  Sym  |  Fn  |     |      |       |
+// |  Ctrl |  GUI | Alt | Tab | Space |     |       | Bkspc | RAlt | Alt |  GUI |  Ctrl |
+// |       |      |     | Nav |       | Sym |  Num  |       |  Fn  |     |      |       |
 // +-------+------+-----+-----+-------+-----+-------+-------+------+-----+------+-------+
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_GRV,  KC_Q, KC_W, KC_E, KC_R,   KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    MOUSE_T,
-    LT_TABG, QM_A,    QM_S,    QM_D,   QM_F,   KC_G,   KC_H,   QM_J,    QM_K,    QM_L,    QM_SCLN, KC_NO,
-    OS_LSFT, QM_Z,    QM_X,    QM_C,   QM_V,   KC_B,   KC_N,   QM_M,    QM_COMM, QM_DOT,  QM_SLSH, OS_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT, LT_TAB, LT_SPC, LT_ESC, LT_ENT, LT_BSPC, LT_RALT, OS_LALT, OS_RGUI, OS_RCTL
+    KC_GRV,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    MOUSE_T,
+    LT_ESCG, QM_A,    QM_S,    QM_D,   QM_F,   KC_G,   KC_H,   QM_J,    QM_K,    QM_L,    QM_SCLN, KC_ENT,
+    TD_LSFT, QM_Z,    QM_X,    QM_C,   QM_V,   KC_B,   KC_N,   QM_M,    QM_COMM, QM_DOT,  QM_SLSH, TD_RSFT,
+    KC_LCTL, KC_LGUI, KC_LALT, LT_TAB, KC_SPC, NUM,    SYM,    KC_BSPC, LT_RALT, OS_LALT, OS_RGUI, OS_RCTL
     ),
 #endif
