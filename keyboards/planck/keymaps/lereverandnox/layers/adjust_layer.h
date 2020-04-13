603d69fe4f34c6964964f05a3418ab2de8aca2ce
LeReverandNox <lereverandnox@gmail.com>
2020-04-13 22:38:20 +0200

    Add bindings dis/enable GUI keys for gaming purpose

--- keyboards/planck/keymaps/lereverandnox/layers/adjust_layer.h
+++ keyboards/planck/keymaps/lereverandnox/layers/adjust_layer.h
@@ -5,7 +5,7 @@
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |Mu Mod|Ck Rst|MidiTo|Ag Tog|NKRO T|      |      |      |      |      |Reset |EepRst|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Voice+|Ck P+ |      |      |      |      |      |      |      |      |      |Debug |
+ * |Voice+|Ck P+ |GUIOff|GUIOn |      |      |      |      |      |      |      |Debug |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |Voice-|Ck P- |      |      |      |      |      |      |Plover|Qwerty|Gaming|Dvorak|
  * `-----------------------------------------------------------------------------------'
@@ -13,6 +13,6 @@
 [_ADJUST] = LAYOUT_planck_grid(
     MU_TOG, CK_TOGG, AU_TOG,  RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_MAKE,
     MU_MOD, CK_RST,  MI_TOG,  AG_TOGG, NKR_TOG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET,   EEP_RST,
-    MUV_IN, CK_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DEBUG,
+    MUV_IN, CK_UP,   GUI_OFF, GUI_ON,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DEBUG,
     MUV_DE, CK_DOWN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, PLOVER,  QWERTY,  GAMING,  DVORAK
     ),
