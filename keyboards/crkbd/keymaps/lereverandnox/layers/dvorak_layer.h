cf81fef19e37a467943b813e7dad48a3b347bcc3
LeReverandNox <lereverandnox@gmail.com>
2020-04-14 15:07:13 +0200

    Big clean up on unused layers for the Crkbd

--- keyboards/crkbd/keymaps/lereverandnox/layers/dvorak_layer.h
+++ keyboards/crkbd/keymaps/lereverandnox/layers/dvorak_layer.h
@@ -1,43 +1,4 @@
 // clang-format off
-#ifdef OUTER_COLS
-// +-------+------+-----+-----+-------+-----+-------+-------+------+-----+------+-------+
-// | Mouse |   '  |  ,  |  .  |   P   |  Y  |   F   |   G   |   C  |  R  |   L  | Mouse |
-// +-------+------+-----+-----+-------+-----+-------+-------+------+-----+------+-------+
-// | Media |   A  |  O  |  E  |   U   |  I  |   D   |   H   |  T   |  N  |   S  |       |
-// |       | Ctrl | GUI | Alt | Shift |     |       | Shift |  Alt | GUI | Ctrl |       |
-// +-------+------+-----+-----+-------+-----+-------+-------+------+-----+------+-------+
-// |       |   ;  |  Q  |  J  |   K   |  X  |   B   |   M   |   W  |  V  |   Z  |       |
-// +-------+------+-----+-----+-------+-----+-------+-------+------+-----+------+-------+
-// |       |      |     | Esc | Space | Tab | Enter | Bkspc | RAlt |     |      |       |
-// |       |      |     | Nav |  Sym  | Num |  Num  |  Sym  |      |     |      |       |
-// +-------+------+-----+-----+-------+-----+-------+-------+------+-----+------+-------+
-
-[_DVORAK] = LAYOUT(
-    MOUSE_T, KC_QUOT, KC_COMM, KC_DOT, KC_P,   KC_Y,   KC_F,   KC_G,    KC_C,   KC_R, KC_L, MOUSE_T,
-    MEDIA,   DM_A,    DM_O,    DM_E,   DM_U,   KC_I,   KC_D,   DM_H,    DM_T,   DM_N, DM_S, XXXXXXX,
-    XXXXXXX, KC_SCLN, KC_Q,    KC_J,   KC_K,   KC_X,   KC_B,   KC_M,    KC_W,   KC_V, KC_Z, XXXXXXX,
-                               LT_ESC, LT_SPC, LT_TAB, LT_ENT, LT_BSPC, KC_RALT),
-#endif
-
-#ifdef INNER_COLS
-// +------+-----+-----+-------+-----+-------+-------+-------+-------+-----+-----+------+
-// |   '  |  ,  |  .  |   P   |  Y  | Mouse | Mouse |   F   |   G   | C   |  R  |   L  |
-// +------+-----+-----+-------+-----+-------+-------+-------+-------+-----+-----+------+
-// |   A  |  O  |  E  |   U   |  I  | Media |       |   D   |   H   | T   |  N  |   S  |
-// | Ctrl | GUI | Alt | Shift |     |       |       |       | Shift | Alt | GUI | Ctrl |
-// +------+-----+-----+-------+-----+-------+-------+-------+-------+-----+-----+------+
-// |   ;  |  Q  |  J  |   K   |  X  |       |       |   B   |   M   | W   |  V  |   Z  |
-// +------+-----+-----+-------+-----+-------+-------+-------+-------+-----+-----+------+
-// |      |     |     | Space | Esc | Tab   | RAlt  | Enter | Bkspc |     |     |      |
-// |      |     |     |  Sym  | Num | Nav   |       |  Num  |  Sym  |     |     |      |
-// +------+-----+-----+-------+-----+-------+-------+-------+-------+-----+-----+------+
-[_DVORAK] = LAYOUT(
-    KC_QUOT, KC_COMM, KC_DOT, KC_P,    KC_Y,   MOUSE_T, MOUSE_T, KC_F,   KC_G,   KC_C, KC_R, KC_L,
-    DM_A,    DM_O,    DM_E,   DM_U,    KC_I,   MEDIA,   XXXXXXX, KC_D,   DM_H,   DM_T, DM_N, DM_S,
-    KC_SCLN, KC_Q,    KC_J,   KC_K,    KC_X,   XXXXXXX, XXXXXXX, KC_B,   KC_M,   KC_W, KC_V, KC_Z,
-                              LT_SPC,  LT_ESC, LT_TAB,  KC_RALT, LT_ENT, LT_BSPC),
-#endif
-
 #ifdef OUTER_CLASSICAL
 // +-------+------+-----+-----+-------+-----+-------+-------+------+-----+------+-------+
 // |   `   |   '  |  ,  |  .  |   P   |  Y  |   F   |   G   |   C  |  R  |   L  | Mouse |
--- keyboards/crkbd/keymaps/lereverandnox/layers/qwerty_layer.h
+++ keyboards/crkbd/keymaps/lereverandnox/layers/qwerty_layer.h
@@ -1,25 +1,4 @@
 // clang-format off
-#ifdef OUTER_COLS
-/* Qwerty
- * ,-----------------------------------------------------------------------------------.
- * |Mouse |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |Mouse |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Media |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |      |
- * |      | Ctrl | GUI  | Alt  |Shift |      |      |Shift |  Alt |  GUI | Ctrl |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      | Esc  |  Spc | Tab  |Enter | Bksp | RAlt |      |      |      |
- * |      |      |      | Nav  | Sym  | Num  | Num  | Sym  |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
-[_QWERTY] = LAYOUT(
-    MOUSE_T, KC_Q, KC_W, KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    MOUSE_T,
-    MEDIA,   QM_A, QM_S, QM_D,   QM_F,   KC_G,   KC_H,   QM_J,    QM_K,    QM_L,   QM_SCLN, XXXXXXX,
-    XXXXXXX, KC_Z, KC_X, KC_C,   KC_V,   KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, XXXXXXX,
-                         LT_ESC, LT_SPC, LT_TAB, LT_ENT, LT_BSPC, KC_RALT),
-#endif
-
 #ifdef OUTER_CLASSICAL
 // +-------+------+-----+-----+-------+-----+-------+-------+------+-----+------+-------+
 // |   `   |   Q  |  W  |  E  |   R   |  T  |   Y   |   U   |   I  |  O  |   P  | Mouse |
