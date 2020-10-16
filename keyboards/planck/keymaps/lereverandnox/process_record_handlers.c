05b0b26d63795ce4516ffde34938e48f2f115cc5
LeReverandNox <lereverandnox@gmail.com>
2020-10-16 23:30:43 +0200

    Update KC_MAKE handler to work with custom command

--- keyboards/planck/keymaps/lereverandnox/process_record_handlers.c
+++ keyboards/planck/keymaps/lereverandnox/process_record_handlers.c
@@ -153,7 +153,7 @@ bool KC_MAKE_handler(keyrecord_t *record) {
     if (!record->event.pressed) {
         uint8_t mods = get_mods();
         clear_mods();
-        send_string_with_delay_P(PSTR("ALLOW_WARNINGS=yes make " QMK_KEYBOARD ":" QMK_KEYMAP), 10);
+        send_string_with_delay_P(PSTR("qmk_build " QMK_KEYBOARD ":" QMK_KEYMAP), 10);
         if (mods & MOD_MASK_SHIFT) {
             // RESET board for flashing if SHIFT held or tapped with KC_MAKE
             send_string_with_delay_P(PSTR(":dfu-util"), 10);
