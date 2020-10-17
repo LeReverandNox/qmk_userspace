b33670a4c5e64e1da0376fc6405a94669ff29ad6
LeReverandNox <lereverandnox@gmail.com>
2020-10-17 22:11:27 +0200

    Update sym layers, remove duplicates `/'

--- keyboards/planck/keymaps/lereverandnox/layers/sym_layer.h
+++ keyboards/planck/keymaps/lereverandnox/layers/sym_layer.h
@@ -24,16 +24,16 @@
  * ,-----------------------------------------------------------------------------------
  * |   ~  |   "  |      |  *   |  &   |  (   |  )   |   +  |   _  |   |  |   ?  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   `  |   '  |  ^   |  %   |  $   |  [   |  ]   |   =  |   -  |   \  |   /  |      |
+ * |      |      |  ^   |  %   |  $   |  [   |  ]   |   =  |   -  |   \  |   /  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |   :  |  #   |  @   |  !   |  {   |  }   |   <  |   >  |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Caps |      |      |      | Del  |      |      | Del  |      |      |      |      |
+ * |      |      |      |      | Del  |      |      | Del  |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_SYM] = LAYOUT_planck_grid(
-    KC_TILD, KC_DQUO, XXXXXXX, KC_ASTR, KC_AMPR, KC_LPRN, KC_RPRN, KC_PLUS, KC_UNDS, KC_PIPE, KC_QUES, XXXXXXX,
-    KC_GRV,  KC_QUOT, KC_CIRC, KC_PERC, KC_DLR,  KC_LBRC, KC_RBRC, KC_EQL,  KC_MINS, KC_BSLS, KC_SLSH, _______,
+    KC_TILD, KC_DQUO, XXXXXXX, KC_ASTR, KC_AMPR, KC_LPRN, KC_RPRN, KC_PLUS, KC_UNDS, KC_PIPE, KC_QUES, _______,
+    _______, XXXXXXX, KC_CIRC, KC_PERC, KC_DLR,  KC_LBRC, KC_RBRC, KC_EQL,  KC_MINS, KC_BSLS, KC_SLSH, _______,
     _______, KC_COLN, KC_HASH, KC_AT,   KC_EXLM, KC_LCBR, KC_RCBR, KC_LABK, KC_RABK, XXXXXXX, XXXXXXX, _______,
     _______, _______, _______, _______, KC_DEL,  _______, _______, KC_DEL,  _______, _______, _______, _______
     ),
