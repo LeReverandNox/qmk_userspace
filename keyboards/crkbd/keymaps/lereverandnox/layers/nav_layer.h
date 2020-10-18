94976dc29d3d483c0deec1a510d6e88d8cf17e17
LeReverandNox <lereverandnox@gmail.com>
2020-10-18 23:06:12 +0200

    Add Scroll Lock / Pause in Nav layer

--- keyboards/crkbd/keymaps/lereverandnox/layers/nav_layer.h
+++ keyboards/crkbd/keymaps/lereverandnox/layers/nav_layer.h
@@ -2,7 +2,7 @@
 #ifdef OUTER_CLASSICAL
 /* Nav
  * ,-----------------------------------------------------------------------------------
- * |      |      |      |      |      |      |      | Home |Pg Up |      |      |Mouse |
+ * |      |      |      |ScLck |Pause |      |      | Home |Pg Up |      |      |Mouse |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Caps |Again |      |      |      |      |      | Left | Down |  Up  |Right |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -12,7 +12,7 @@
  * `-----------------------------------------------------------------------------------'
  */
 [_NAV] = LAYOUT(
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGUP,  XXXXXXX, XXXXXXX, MOUSE_T,
+    XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, KC_PAUS, XXXXXXX, XXXXXXX, KC_HOME, KC_PGUP,  XXXXXXX, XXXXXXX, MOUSE_T,
     KC_CAPS, KC_AGIN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, _______,
     _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, XXXXXXX, XXXXXXX, KC_END,  KC_PGDN,  XXXXXXX, XXXXXXX, _______,
                                _______, _______, _______, _______, KC_INS,  _______),
