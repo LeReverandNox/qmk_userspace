9ae4ec0297aeb38f44fbbda15b9db9c9c99919a9
LeReverandNox <lereverandnox@gmail.com>
2020-10-18 21:32:14 +0200

    Remove transparency in mouse layers

--- keyboards/crkbd/keymaps/lereverandnox/layers/mouse_layer.h
+++ keyboards/crkbd/keymaps/lereverandnox/layers/mouse_layer.h
@@ -11,7 +11,7 @@
  * `-----------------------------------------------------------------------------------'
  */
 [_MOUSE] = LAYOUT(
-    _______, XXXXXXX, KC_BTN2, KC_BTN3, KC_BTN1, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN3, KC_BTN2, XXXXXXX, _______,
+    XXXXXXX, XXXXXXX, KC_BTN2, KC_BTN3, KC_BTN1, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN3, KC_BTN2, XXXXXXX, XXXXXXX,
     KC_BTN4, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN5, KC_BTN4, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN5,
-    _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX, XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______,
-                               _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______),
+    XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX, XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX,
+                               _______, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX),
--- keyboards/planck/keymaps/lereverandnox/layers/mouse_layer.h
+++ keyboards/planck/keymaps/lereverandnox/layers/mouse_layer.h
@@ -11,8 +11,8 @@
  * `-----------------------------------------------------------------------------------'
  */
 [_MOUSE] = LAYOUT_planck_grid(
-    _______, XXXXXXX, KC_BTN2, KC_BTN3, KC_BTN1, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN3, KC_BTN2, XXXXXXX, _______,
+    XXXXXXX, XXXXXXX, KC_BTN2, KC_BTN3, KC_BTN1, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN3, KC_BTN2, XXXXXXX, XXXXXXX,
     KC_BTN4, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN5, KC_BTN4, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN5,
-    _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX, XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______,
-    _______, _______, _______, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______, _______, _______, _______
+    XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX, XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX,
+    _______, _______, _______, _______, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX, _______, _______, _______
     ),
