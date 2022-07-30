247485051d0f5930bee7189e1aaa10427da1ac69
LeReverandNox <lereverandnox@gmail.com>
2022-07-30 21:00:31 +0200

    Fill Adjust layer (Kyria)

--- keyboards/splitkb/kyria/keymaps/lereverandnox/layers/adjust_layer.h
+++ keyboards/splitkb/kyria/keymaps/lereverandnox/layers/adjust_layer.h
@@ -1,47 +1,25 @@
 // clang-format off
-/* Adjust
- *                         v------------------------RGB CONTROL----------------------------v
- * ,-----------------------------------------------------------------------------------------------.
- * |       |       |       | RGB   |RGBMOD | HUE+  | HUE-  | SAT+  | SAT-  |BRGTH+ |BRGTH- | Make  |
- * |       |       |       |       |       |       |       |       |       |       |       |       |
- * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- * |       |       |       |Ag Tog |NKRO T |       |       |       |       |       |Reset  |EepRst |
- * |       |       |       |       |       |       |       |       |       |       |       |       |
- * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- * |       |       |GUIOff |GUIOn  |       |       |       |       |       |       |       |Debug  |
- * |       |       |       |       |       |       |       |       |       |       |       |       |
- * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- * |       |       |       |       |       |       |       |       |       |       |Qwerty |Dvorak |
- * |       |       |       |       |       |       |       |       |       |       |       |       |
- * `-----------------------------------------------------------------------------------------------'
- */
-/* [_ADJUST] = LAYOUT( */
-/*     XXXXXXX,  XXXXXXX, XXXXXXX, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_MAKE, */
-/*     XXXXXXX,  XXXXXXX, XXXXXXX, AG_TOGG, NKR_TOG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET,   EEP_RST, */
-/*     XXXXXXX,  XXXXXXX, GUI_OFF, GUI_ON,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DEBUG, */
-/*     XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  QWERTY,  DVORAK */
-/* ), */
-
 /*
  * Adjust
  *
+ * v----------------------RGB CONTROL--------------------v
  * ,-----------------------------------------------------.                                      ,-----------------------------------------------------.
- * |        |        |        |        |        |        |                                      |        |        |        |        |        |        |
+ * | Toggle | Mode + | HUE +  | SAT +  | BRI +  | SPE +  |                                      |        |        |        |        |        |  Make  |
  * |        |        |        |        |        |        |                                      |        |        |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------|                                      |--------+--------+--------+--------+--------+--------|
- * |        |        |        |        |        |        |                                      |        |        |        |        |        |        |
+ * |        | Mode - | HUE -  | SAT -  | BRI -  | SPE -  |                                      |        |        |        |        | Reset  | EepRst |
  * |        |        |        |        |        |        |                                      |        |        |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------|-----------------.  ,-----------------|--------+--------+--------+--------+--------+--------|
  * |        |        |        |        |        |        |        |        |  |        |        |        |        |        |        |        |        |
  * |        |        |        |        |        |        |        |        |  |        |        |        |        |        |        |        |        |
  * `--------+--------+--------+--------+--------+--------|--------|--------|  |--------|--------|--------+--------+--------+--------+--------+--------'
- *                            |ADJ_EN1 |        |        |        |        |  |        |        |        |        |ADJ_EN2 |
+ *                            |ADJ_EN1 |        |        |        |        |  |        |        | Qwerty | Dvorak |ADJ_EN2 |
  *                            |        |        |        |        |        |  |        |        |        |        |        |
  *                            `--------------------------------------------'  `--------------------------------------------'
  */
 [_ADJUST] = LAYOUT(
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,                                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MAKE,
+    XXXXXXX, RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD,                                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, EE_CLR,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, NKR_TOG, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                ADJ_EN1, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, QWERTY,  DVORAK,  ADJ_EN2
 ),
