de8064c5d0f0280238c9a8ec2420b71cc191a227
LeReverandNox <lereverandnox@gmail.com>
2022-07-30 22:12:25 +0200

    Fix wrong comparison for TAP_DANCE_ENABLE

--- keyboards/splitkb/kyria/keymaps/lereverandnox/rules.mk
+++ keyboards/splitkb/kyria/keymaps/lereverandnox/rules.mk
@@ -7,7 +7,7 @@ MOUSEKEY_ENABLE = yes
 SRC += helpers.c
 SRC += process_record_handlers.c
 SRC += user_functions.c
-ifeq ($(strip $(TAP_DANCE_ENABLE)), true)
+ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
 SRC += tapdances.c
 SRC += tapdances_handlers.c
 endif
