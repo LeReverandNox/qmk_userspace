883fade006b67b95efbfe04e8307a3973e473684
r.laidet <r.laidet@lectra.com>
2022-09-02 18:03:11 +0200

    Use LTO_ENABLE instead of lto flag (Kyria)

--- keyboards/splitkb/kyria/keymaps/lereverandnox/rules.mk
+++ keyboards/splitkb/kyria/keymaps/lereverandnox/rules.mk
@@ -4,6 +4,7 @@ ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 TAP_DANCE_ENABLE = no
 MOUSEKEY_ENABLE = yes
+LTO_ENABLE      = yes
 SRC += helpers.c
 SRC += process_record_handlers.c
 SRC += user_functions.c
@@ -11,6 +12,3 @@ ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
 SRC += tapdances.c
 SRC += tapdances_handlers.c
 endif
-
-# Firmware size reduction
-EXTRAFLAGS += -flto
