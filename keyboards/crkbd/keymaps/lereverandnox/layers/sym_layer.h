9b691352f206023be060a668a1844694011bc656
LeReverandNox <lereverandnox@gmail.com>
2024-03-27 19:15:22 +0100

    Remove second KC_DEL from symbols layers

--- keyboards/crkbd/keymaps/lereverandnox/layers/sym_layer.h
+++ keyboards/crkbd/keymaps/lereverandnox/layers/sym_layer.h
@@ -12,7 +12,7 @@
  * |        |    :   |    #   |    @   |    !   |    {   |                    |    }   |    <   |    >   |        |        |        |
  * |        |        |        |        |        |        |                    |        |        |        |        |        |        |
  * `--------+--------+--------+--------+--------+--------|--------.  ,--------|--------|--------|--------+--------+--------+--------'
- *                                     |        |   Del  |        |  |        |   Del  |        |
+ *                                     |        |        |        |  |        |   Del  |        |
  *                                     |        |        |        |  |        |        |        |
  *                                     `--------------------------'  `--------------------------'
  */
@@ -20,5 +20,5 @@
     KC_TILD, KC_DQUO, XXXXXXX, KC_ASTR, KC_AMPR, KC_LPRN,                      KC_RPRN, KC_PLUS, KC_UNDS, KC_PIPE, KC_QUES, XXXXXXX,
     XXXXXXX, XXXXXXX, KC_CIRC, KC_PERC, KC_DLR,  KC_LBRC,                      KC_RBRC, KC_EQL,  KC_MINS, KC_BSLS, KC_SLSH, XXXXXXX,
     XXXXXXX, KC_COLN, KC_HASH, KC_AT,   KC_EXLM, KC_LCBR,                      KC_RCBR, KC_LABK, KC_RABK, XXXXXXX, XXXXXXX, XXXXXXX,
-                                        XXXXXXX, KC_DEL,  _______,    _______, KC_DEL,  XXXXXXX
+                                        XXXXXXX, _______, _______,    _______, KC_DEL,  XXXXXXX
 ),
